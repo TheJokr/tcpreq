6735c5a87e92a6c255d3371828807c3d380fdfd3
Leo Blöcher <leo.bloecher@rwth-aachen.de>
2019-03-27 18:34:19 +0100

    Added CLI option for test selection

--- tcpreq/opts.py
+++ tcpreq/opts.py
@@ -1,9 +1,10 @@
-from typing import Tuple
+from typing import Type, Tuple
 import argparse
 from urllib.parse import urlparse
 import ipaddress
 
 from .types import AnyIPAddress
+from . import tests
 
 
 def _parse_target(value: str) -> Tuple[AnyIPAddress, int]:
@@ -14,6 +15,17 @@ def _parse_target(value: str) -> Tuple[AnyIPAddress, int]:
     return ipaddress.ip_address(urlres.hostname), urlres.port
 
 
+def _parse_test(value: str) -> Type[tests.BaseTest]:
+    try:
+        cls = getattr(tests, value)
+        if issubclass(cls, tests.BaseTest):
+            return cls  # type: ignore
+        else:
+            raise ValueError("'{}' is not a subclass of BaseTest".format(value))
+    except AttributeError as e:
+        raise ValueError(str(e)) from e
+
+
 parser = argparse.ArgumentParser(
     prog=__package__, description="Perform a variety of tests on remote TCP hosts",
     epilog="Use @file to include a file's content as if it was part of the command line",
@@ -21,3 +33,6 @@ parser = argparse.ArgumentParser(
 )
 parser.add_argument("target", nargs="+", type=_parse_target,
                     help="a target to test (e.g. 203.0.113.1:8753 or [2001:db8::1]:4763)")
+parser.add_argument("-T", "--test", action="append", type=_parse_test, metavar="TestClass",
+                    help="A test to perform (subclass of tcpreq.tests.BaseTest). "
+                         "May be specified multiple times.")
--- tcpreq/tests/__init__.py
+++ tcpreq/tests/__init__.py
@@ -1,1 +1,6 @@
+from typing import Type, Tuple
+
 from .base import BaseTest
+
+# List of tests to perform if no explicit list of tests is given
+DEFAULT_TESTS: Tuple[Type[BaseTest], ...] = ()
