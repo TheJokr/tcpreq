5c7a8bc1438094eb4109d67ba8114503948deb9b
Leo Blöcher <leo.bloecher@rwth-aachen.de>
2019-04-13 17:20:55 +0200

    Added TTL parameter to BaseTest.send

--- tcpreq/tests/base.py
+++ tcpreq/tests/base.py
@@ -1,5 +1,6 @@
 from abc import abstractmethod
-from typing import Generic, Awaitable, Tuple, Union, Optional
+from typing import Generic, ClassVar, Awaitable, Tuple, Union, Optional
+import math
 import asyncio
 
 from .result import TestResult
@@ -9,6 +10,13 @@ from ..tcp import Segment
 
 class BaseTest(Generic[IPAddressType]):
     """Abstract base class for all tests."""
+    _HOP_LIMIT: ClassVar[int] = 20
+
+    # Make sure _HOP_LIMIT fits into 5 bits (to save it in the IPv4 ID field 3 times).
+    assert math.floor(math.log2(_HOP_LIMIT) + 1) <= 5
+    # Make sure _HOP_LIMIT can be encoded into IHL+options (39 possible non-zero values).
+    assert _HOP_LIMIT <= 39
+
     def __init__(self, src: Tuple[IPAddressType, int], dst: Tuple[IPAddressType, int],
                  loop: asyncio.AbstractEventLoop = None) -> None:
         if loop is None:
@@ -21,10 +29,13 @@ class BaseTest(Generic[IPAddressType]):
                                   "Tuple[Segment, IPAddressType, int]]]"] = None
         self._loop = loop
 
-    def send(self, seg: Segment) -> Awaitable[None]:
-        if self.send_queue is None:
-            raise RuntimeError("Test is not registered with any multiplexer")
-        return self.send_queue.put((seg, self.dst[0]))
+    def send(self, seg: Segment, ttl: int = None) -> Awaitable[None]:
+        assert self.send_queue is not None, "Test is not registered with any multiplexer"
+        if ttl is None:
+            return self.send_queue.put((seg, self.dst[0]))
+
+        assert 1 <= ttl <= self._HOP_LIMIT
+        return self.send_queue.put((seg, self.dst[0], ttl))
 
     def receive(self, timeout: float) -> Awaitable[Segment]:
         return asyncio.wait_for(self.recv_queue.get(), timeout, loop=self._loop)
