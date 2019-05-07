ea0929300da1a6953612c99efa35f71583abbf80
Leo Blöcher <leo.bloecher@rwth-aachen.de>
2019-05-07 18:51:30 +0200

    Added explicit assertion to check_window

--- tcpreq/tcp/__init__.py
+++ tcpreq/tcp/__init__.py
@@ -2,9 +2,9 @@ from .segment import Segment
 from .options import end_of_options, noop as noop_option, MSSOption
 
 
-# seq, left, right are assumed to be less than 2^32
 def check_window(seq: int, left: int, right: int) -> bool:
     """Check whether seq is contained in [left, right) (modulo 2^32)."""
+    assert all(i < 2**32 for i in (seq, left, right)), "Arguments must be within [0, 2^32)"
     if left <= right:
         return left <= seq < right
     else:
