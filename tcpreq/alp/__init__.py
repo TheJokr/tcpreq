4911e05439fd2187a5d18d9aa16178c7b5e9cc67
Leo Blöcher <leo.bloecher@rwth-aachen.de>
2019-05-09 17:49:28 +0200

    Added ALP module port mapping

--- tcpreq/alp/__init__.py
+++ tcpreq/alp/__init__.py
@@ -1,1 +1,10 @@
+from typing import Type, Dict
+
+from .base import BaseProtocol
 from .http import HTTPProtocol
+
+PORT_MAP: Dict[int, Type[BaseProtocol]] = {}
+for proto in (HTTPProtocol,):
+    for port in proto.ports:  # type: ignore
+        assert port not in PORT_MAP, "Port {} is assigned to multiple ALP classes".format(port)
+        PORT_MAP[port] = proto  # type: ignore
