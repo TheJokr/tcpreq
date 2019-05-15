f264b5203f242245de3ba171f36143559edb3ec3
Leo Blöcher <leo.bloecher@rwth-aachen.de>
2019-05-15 18:40:11 +0200

    Added TLS ALP module

--- tcpreq/alp/__init__.py
+++ tcpreq/alp/__init__.py
@@ -2,9 +2,10 @@ from typing import Type, Dict
 
 from .base import BaseProtocol
 from .http import HTTPProtocol
+from .tls import TLSProtocol
 
 PORT_MAP: Dict[int, Type[BaseProtocol]] = {}
-for proto in (HTTPProtocol,):
+for proto in (HTTPProtocol, TLSProtocol):
     for port in proto.ports:  # type: ignore
         assert port not in PORT_MAP, "Port {} is assigned to multiple ALP classes".format(port)
         PORT_MAP[port] = proto  # type: ignore
