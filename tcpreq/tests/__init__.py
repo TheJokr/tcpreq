b2a32461ea1ab16c7916390ae516c8df6346d9e4
Leo Blöcher <leo.bloecher@rwth-aachen.de>
2019-06-04 17:23:02 +0200

    Added header option tests to tests module

--- tcpreq/tests/__init__.py
+++ tcpreq/tests/__init__.py
@@ -4,6 +4,7 @@ from .base import BaseTest
 from .result import TestResult, TEST_PASS, TEST_UNK, TEST_FAIL
 from .checksum import ChecksumTest
 from .rst_ack import RSTACKTest
+from .options import OptionSupportTest, UnknownOptionTest, IllegalLengthOptionTest
 
 # List of tests to perform if no explicit list of tests is given
-DEFAULT_TESTS: Tuple[Type[BaseTest], ...] = (ChecksumTest,)
+DEFAULT_TESTS: Tuple[Type[BaseTest], ...] = (ChecksumTest, OptionSupportTest, UnknownOptionTest)
